d8636ad17919c801f2ebbe29c7dc649464649f5e
Alexander Kvenvolden <kvenclassical@comcast.net>
2018-02-06 09:26:54 -0800

    Recreated test project with XUnit.

--- AnyBind/AnyBind.Tests/Properties/AssemblyInfo.cs
+++ AnyBind/AnyBind.Tests/Properties/AssemblyInfo.cs
@@ -1,7 +1,10 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("AnyBind.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -11,10 +14,23 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-[assembly: Guid("09929117-c40e-4801-968d-e685d0018f92")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("03cf4a00-cd13-4138-a4e7-29b2f92200cc")]
 
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AnyBind/AnyBind.Tests/ReflectionHelpersTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnyBind.Tests
+{
+    public class ReflectionHelpersTests
+    {
+        class TestClass1
+        {
+            public int Prop1 { get; set; } = 5;
+            private int Field1 = 10;
+        }
+
+        class TestClass2 : TestClass1
+        {
+            public int Prop2 { get; set; } = 3;
+            private int Field2 = 7;
+            public TestClass1 Class1 = new TestClass1();
+        }
+
+        [Fact]
+        public void TryGetMemberValue_Succeess()
+        {
+            var class2 = new TestClass2();
+            object result;
+            ReflectionHelpers.TryGetMemberValue(class2, typeof(TestClass2).GetTypeInfo(), "Prop1", out result);
+            ReflectionHelpers.TryGetMemberValue(class2, typeof(TestClass2).GetTypeInfo(), "Prop2", out result);
+            ReflectionHelpers.TryGetMemberValue(class2, typeof(TestClass2).GetTypeInfo(), "Field1", out result);
+            ReflectionHelpers.TryGetMemberValue(class2, typeof(TestClass2).GetTypeInfo(), "Field2", out result);
+
+            var parent = BindingManager.GetParent(class2, typeof(TestClass2).GetTypeInfo(), "Class1.Field1");
+        }
+    }
+}
