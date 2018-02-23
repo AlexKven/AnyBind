7bdfb7523b22cde87b67f0168bc9a803fe6c5717
Alexander Kvenvolden <kvenclassical@comcast.net>
2018-02-22 22:51:31 -0800

    Created GeneralSubscribable.

---
+++ AnyBind/AnyBind.Tests/Internal/GeneralSubscribableTests.cs
@@ -0,0 +1,35 @@
+﻿using AnyBind.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnyBind.Tests.Internal
+{
+    public class GeneralSubscribableTests
+    {
+        public class Test1
+        {
+            public int Int1 { get; set; } = 1;
+            public int Int2 { get; set; } = 4;
+            public int Int3 { get; set; } = 9;
+        }
+
+        public class Test2
+        {
+            public Test1 T1 { get; set; } = new Test1();
+            public string Str1 { get; set; } = "One";
+            public string Str2 { get; set; } = "Two";
+            public string Str3 { get; set; } = "Three";
+        }
+
+        [Fact]
+        public void SimpleCase()
+        {
+            var gs = new GeneralSubscribable(new Test2());
+
+        }
+    }
+}
---
+++ AnyBind/AnyBind/Internal/GeneralSubscribable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AnyBind.Internal
+{
+    internal class GeneralSubscribable : ISubscribable
+    {
+        private object Instance { get; }
+        private Type InstanceType { get; }
+        private TypeInfo InstanceTypeInfo { get; }
+        private List<string> Properties { get; } = new List<string>();
+
+        public GeneralSubscribable(object instance)
+        {
+            Instance = instance;
+            InstanceType = instance.GetType();
+            InstanceTypeInfo = InstanceType.GetTypeInfo();
+        }
+
+        public IEnumerable<string> SubscribableProperties => Properties;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public object GetPropertyValue(string propertyPath)
+        {
+            string propertyName = propertyPath.Substring(propertyPath.LastIndexOf('.') + 1);
+            object parent = Instance;
+            parent = ReflectionHelpers.GetParentOfSubentity(parent, InstanceTypeInfo, propertyPath);
+            if (parent == null)
+                return null;
+
+            return ReflectionHelpers.SearchTypeAndBase(parent.GetType().GetTypeInfo(), t => t.DeclaredProperties.FirstOrDefault(pi => pi.Name == propertyName))?.GetValue(parent);
+        }
+
+        public void RaisePropertyChanged(PropertyChangedEventArgs e)
+        {
+            Instance.RaiseEvent("PropertyChanged", e);
+        }
+    }
+}
