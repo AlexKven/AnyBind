3620e64f92b84dc1dcedc23876fcb4e23ced844e
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-05-12 06:59:30 -0700

    Created tests for new instance adapters. Need to fix some of them.

---
+++ AnyBind/AnyBind.Tests/Unit Tests/Adapters/NotifyPropertyChangedInstanceAdapterTests.cs
@@ -0,0 +1,76 @@
+﻿using AnyBind.Adapters;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnyBind.Tests.Unit_Tests.Adapters
+{
+    public class NotifyPropertyChangedInstanceAdapterTests
+    {
+        Mock<INotifyPropertyChanged> MockObject = new Mock<INotifyPropertyChanged>();
+        private NotifyPropertyChangedInstanceAdapter Adapter;
+
+        public NotifyPropertyChangedInstanceAdapterTests()
+        {
+            Adapter = new NotifyPropertyChangedInstanceAdapter(MockObject.Object);
+        }
+
+        [Theory]
+        [InlineData()]
+        [InlineData("One")]
+        [InlineData("One", "Two", "Three")]
+        public void NotifyPropertyChangedInstanceAdapter_Success_SubscribeProperties(params string[] propertyNames)
+        {
+            // Arrange, Act
+            var result = Adapter.SubscribeToProperties(propertyNames);
+
+            // Assert
+            Assert.True(result.SequenceEqual(propertyNames));
+        }
+
+        [Fact]
+        public void NotifyPropertyChangedInstanceAdapter_AdapterEventRaised()
+        {
+            // Arrange
+            var raisedCount = 0;
+            Adapter.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Property")
+                    raisedCount++;
+            };
+            Adapter.SubscribeToProperties("Property");
+
+            // Act
+            MockObject.Raise(o => o.PropertyChanged += null, new PropertyChangedEventArgs("Property"));
+
+            // Assert
+            Assert.Equal(expected: 1, actual: raisedCount);
+        }
+
+        [Fact]
+        public void NotifyPropertyChangedInstanceAdapter_AdapterEventRaised_Unsubscribed()
+        {
+            // Arrange
+            var raisedCount = 0;
+            Adapter.PropertyChanged += (s, e) =>
+            {
+                if (e.PropertyName == "Property")
+                    raisedCount++;
+            };
+            Adapter.SubscribeToProperties("Property");
+
+            // Act
+            MockObject.Raise(o => o.PropertyChanged += null, new PropertyChangedEventArgs("Property"));
+            Adapter.UnsubscribeFromProperties("Property");
+            MockObject.Raise(o => o.PropertyChanged += null, new PropertyChangedEventArgs("Property"));
+
+            // Assert
+            Assert.Equal(expected: 1, actual: raisedCount);
+        }
+    }
+}
---
+++ AnyBind/AnyBind.Tests/Unit Tests/Adapters/ObservableCollectionInstanceAdapterTests.cs
@@ -0,0 +1,72 @@
+﻿using AnyBind.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AnyBind.Tests.Unit_Tests.Adapters
+{
+    public class ObservableCollectionInstanceAdapterTests
+    {
+        ObservableCollection<string> Object = new ObservableCollection<string>();
+        private NotifyPropertyChangedInstanceAdapter Adapter;
+
+        [Theory]
+        [InlineData()]
+        [InlineData("One")]
+        [InlineData("One", "Count*")]
+        [InlineData("Count*")]
+        [InlineData("Count*", "One", "[]*")]
+        [InlineData("[FooBar]")]
+        [InlineData("[1]*", "[2]*")]
+        [InlineData("[1]*", "[2]*", "[Foo]", "Bar")]
+        public void NotifyPropertyChangedInstanceAdapter_Success_SubscribeProperties(params string[] propertyNames)
+        {
+            (string, bool) stringIncluded(string str)
+            {
+                if (str.EndsWith("*"))
+                    return (str.Substring(0, str.Length - 1), true);
+                return (str, false);
+            }
+
+            //Arrange
+            var input = propertyNames.Select(prop => stringIncluded(prop).Item1);
+            var expectedOutput = propertyNames.Select(prop => stringIncluded(prop))
+                .Where(obj => obj.Item2)
+                .Select(obj => obj.Item1);
+            
+            // Act
+            var result = Adapter.SubscribeToProperties(propertyNames);
+
+            // Assert
+            Assert.True(result.SequenceEqual(expectedOutput));
+        }
+
+        [Fact]
+        public void NotifyPropertyChangedInstanceAdapter_EventsRaised_Add()
+        {
+            // Arrange
+            Dictionary<string, int> raiseCounts = new Dictionary<string, int>()
+            { ["Count"] = 0, ["[]"] = 0, ["[1]"] = 0, ["[01]"] = 0, ["[2]"] = 0 };
+            Adapter.PropertyChanged += (s, e) =>
+            {
+                if (raiseCounts.ContainsKey(e.PropertyName))
+                    raiseCounts[e.PropertyName]++;
+            };
+            Adapter.SubscribeToProperties("Property");
+
+            // Act
+            Object.Add("Test1");
+
+            // Assert
+            Assert.Equal(expected: 1, actual: raiseCounts["Count"]);
+            Assert.Equal(expected: 1, actual: raiseCounts["[]"]);
+            Assert.Equal(expected: 1, actual: raiseCounts["[1]"]);
+            Assert.Equal(expected: 1, actual: raiseCounts["[01]"]);
+            Assert.Equal(expected: 0, actual: raiseCounts["[2]"]);
+        }
+    }
+}
