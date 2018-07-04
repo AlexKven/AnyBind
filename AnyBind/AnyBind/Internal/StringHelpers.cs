20038c79aea238f81d98ee92c3d0b97d34f9e130
Alexander Kvenvolden <kvenclassical@gmail.com>
2018-07-04 12:36:25 -0700

    Exploring a way to streamline parsing index lists.

--- AnyBind/AnyBind/Internal/StringHelpers.cs
+++ AnyBind/AnyBind/Internal/StringHelpers.cs
@@ -63,5 +63,11 @@ namespace AnyBind.Internal
                 return null;
             return components.Aggregate(("", ""), (acc, str) => (acc.Item1 == "" ? str : str.StartsWith("[") ? acc.Item1 + str : $"{acc.Item1}.{str}", acc.Item1)).Item2;
         }
+
+        //public static bool IsIndexList(this string str, out IEnumerable<string> indexes)
+        //{
+        //    if (!str.StartsWith()
+        //    List<string> result = new List<string>();
+        //}
     }
 }
